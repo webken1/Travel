65541dca168cb56baf071b546807b2109ce16b39
ken <190038279@qq.com>
2019-06-17 12:55:35 +0800

    12:55

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import fastClick from 'fastclick' //安装并导入fastclick这个插件解决移动端延迟300秒问题
-import './assets/styles/reset.css'  //重置样式
-import './assets/styles/border.css' //解决1像素边框
+import 'styles/reset.css'  //重置样式
+import 'styles/border.css' //解决1像素边框
+import 'styles/iconfont.css'
 fastClick.attach(document.body)  //使用fastclick
 
 Vue.config.productionTip = false
